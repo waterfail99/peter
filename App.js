0b410bd0ca3566f5c374ced4f2a7631b9f8e6f60
pouweisiang <weisiang91@gmail.com>
2021-01-13 17:44:33 +0800

    navigation

--- App.js
+++ App.js
@@ -1,46 +1,42 @@
-import React from "react"
-import 'react-native-gesture-handler';
-import { Button,StyleSheet, Text, View }from "react-native"
-import { NavigationContainer } from "@react-navigation/native"
-import { createStackNavigator } from 'react-navigation-stack';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen({navigation}){
-  return(
-    <View>
+function HomeScreen({navigation}) {
+    console.log("Navigation: ", navigation);
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
-      <Button title="gogogo"
-      onPress={()=> navigation.navigate('Details')}></Button>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
-  )
-}
-function DetailScreen(){
-  return(
-    <View>
-      <Text>Details Screen</Text>
-    </View>
-  )
+  );
 }
 
+function DetailsScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
-export default function App(){
-  
-  return(
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component = {HomeScreen}/>
-      <Stack.Screen name="Details" component = {DetailScreen}/>
-    </Stack.Navigator>
-  </NavigationContainer>
-  )
-  
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home">
+            {props => <HomeScreen {...props}/>}
+        </Stack.Screen>
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-const styles = StyleSheet.create({
-  container:{
-    alignItems:"center",
-    justifyContent: "center",
-  },
-})
 
-
+export default App;
