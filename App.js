455672f5b1bc4daa9ecf0be82aeaaf16730c18fa
waterfail99 <luffychoo09@gmail.com>
2021-01-15 09:33:01 +0800

    new

--- App.js
+++ App.js
@@ -1,53 +1,59 @@
-// React Native Pass Value From One Screen to Another Using React Navigation
-// https://aboutreact.com/react-native-pass-value-from-one-screen-to-another-using-react-navigation/
-
-import 'react-native-gesture-handler';
-
-import React from 'react';
-
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
-import FirstPage from './src/FirstPage';
-import SecondPage from './src/SecondPage';
-
-const Stack = createStackNavigator();
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="FirstPage">
-        <Stack.Screen
-          name="FirstPage"
-          component={FirstPage}
-          options={{
-            title: 'First Page', //Set Header Title
-            headerStyle: {
-              backgroundColor: 'black', //Set Header color
-            },
-            headerTintColor: '#fff', //Set Header text color
-            headerTitleStyle: {
-              fontWeight: 'bold', //Set Header text style
-            },
-          }}
-        />
-        <Stack.Screen
-          name="SecondPage"
-          component={SecondPage}
-          options={{
-            title: 'Second Page', //Set Header Title
-            headerStyle: {
-              backgroundColor: 'black', //Set Header color
-            },
-            headerTintColor: '#fff', //Set Header text color
-            headerTitleStyle: {
-              fontWeight: 'bold', //Set Header text style
-            },
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-export default App;
+import React, { Component } from 'react';  
+import { AppRegistry, FlatList,  
+    StyleSheet, Text, View,Alert } from 'react-native';  
+  
+export default class FlatListBasics extends Component {  
+  
+    renderSeparator = () => {  
+        return (  
+            <View  
+                style={{  
+                    height: 1,  
+                    width: "100%",  
+                    backgroundColor: "#000",  
+                }}  
+            />  
+        );  
+    };  
+    //handling onPress action  
+    getListViewItem = (item) => {  
+        Alert.alert(item.text,item.body);  
+        
+         
+    }  
+  
+    render() {  
+        return (  
+            <View style={styles.container}>  
+                <FlatList  
+                    data={[  
+                      { id: 0, text: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',body:'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto' },
+                      { id: 1, text: 'qui est esse',body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui"},
+                      { id: 2, text: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"},
+                      { id: 3, text: 'eum et est occaecati',body:"ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit" },
+                      { id: 4, text: 'nesciunt quas odio',body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"}
+                    ]}  
+                    renderItem={({item}) =>  
+                        <Text style={styles.item}  
+                              onPress={this.getListViewItem.bind(this, item)}>{item.text}</Text>}  
+                    ItemSeparatorComponent={this.renderSeparator}  
+                />  
+            </View>  
+        );  
+    }  
+}  
+  
+const styles = StyleSheet.create({  
+    container: {  
+      marginTop: 20,
+      flex: 1
+    },  
+    item: {  
+      padding: 15,
+      marginBottom: 5,
+      backgroundColor: 'skyblue'
+    },  
+})  
+  
+  
+AppRegistry.registerComponent('AwesomeProject', () => FlatListBasics);  
