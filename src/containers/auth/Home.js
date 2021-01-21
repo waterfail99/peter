50260c1908e4da45a380b6495d82377a4060eb0a
pouweisiang <weisiang91@gmail.com>
2021-01-21 09:38:35 +0800

    data

---
+++ src/containers/auth/Home.js
@@ -0,0 +1,31 @@
+const PAIR_LIST = [  
+    { base: 'USDT', coin: 'BTC', price: 35083.62, change: 0.65, volume: 23728.33, high: 36360.16, low: 33410, last: 34941.11, decimal: 2, priority: 1, isFeature: true},
+    { base: 'USDT', coin: 'EOS', price: 2.7460, change: 2.59, volume: 23597095, high: 2.8116, low: 2.656, last: 2.74, decimal: 4, priority: 5, isFeature: true},
+    { base: 'USDT', coin: 'ETH', price: 1352.65, change: -3.16, volume: 668705.38, high: 1402.24, low: 1235.71, last: 1354, decimal: 2, priority: 3, isFeature: false},
+    { base: 'USDT', coin: 'BNB', price: 42.0999, change: -0.76, volume: 591269.34, high: 42.6872, low: 40.2, last: 42.15, decimal: 4, priority: 2, isFeature: true},
+    { base: 'USDT', coin: 'XRP', price: 0.29217, change: 2.2, volume: 833009945.7, high: 0.31184, low: 33410.00, last: 0.28, decimal: 5, priority: 4, isFeature: false},
+];
+
+const CURRENCY = {
+    symbol: 'MYR',
+    value: 4.3,
+}
+
+const OPTIONS = [
+    {
+        symbol: '',
+        title: "Favourites"
+    },
+    {
+        symbol: 'USDT',
+        title: "USDT Markets"
+    },
+    {
+        symbol: 'BTC',
+        title: "BTC Markets"
+    },
+    {
+        symbol: 'ETH',
+        title: "ETH Markets"
+    }
+]
